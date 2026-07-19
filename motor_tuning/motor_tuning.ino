1257da6ff06bf0f4c1264c8704e0f1e03d8a5d05
Logan Boswell <loganstuartboswell@gmail.com>
2026-07-18 20:44:51 -0500

    Confirmed that motor velocity can be read

--- motor_tuning/motor_tuning.ino
+++ motor_tuning/motor_tuning.ino
@@ -1,27 +1,39 @@
 #include <ESP32Encoder.h>
 
-ESP32Encoder encoder;
 const int encoderA = 36;
 const int encoderB = 39;
 const int pwm = 18;
 const int dir = 19;
+const int pwmFreq = 20000;
+const int pwmRes = 10;
+const int maxDutyCnt = pow(2, pwmRes) - 1 ;
+const int encoderCPR = 64;
+const float gearRatio = 18.75;
+const float countsPerRev = ((float)encoderCPR) * gearRatio;
+
+ESP32Encoder encoder;
 int duty = 0;
+int64_t currentCount = 0;
+int64_t prevCount = 0;
+float currentVel = 0.0;
+float prevVel = 0.0;
+float kp = 0.0;
+float ki = 0.0;
+float kd = 0.0;
+float currentTime, prevTime;
 
 void setup() {
   Serial.begin(115200);
-  // ESP32Encoder::useInternalWeakPullResistors = puType::up;
   encoder.attachFullQuad(encoderA, encoderB);
   encoder.setCount(0);
 
-  delay(5000);
-
   pinMode(dir, OUTPUT);
   digitalWrite(dir, LOW);
-  // pinMode(pwm, OUTPUT);
-  // digitalWrite(pwm, LOW);
-  ledcAttach(pwm, 20000, 10);
+  ledcAttach(pwm, pwmFreq, pwmRes);
   ledcWrite(pwm, duty);
-  
+
+  currentTime = micros() / 1e6f;
+  prevTime = currentTime;
 }
 
 void loop() {
@@ -29,11 +41,11 @@ void loop() {
     char input = Serial.read();
 
     switch (input) {
-      case 'w': // forward
+      case 'w':
         duty += 10;
         break;
 
-      case 's': // reverse
+      case 's':
         duty -= 10;
         break;
     }
@@ -44,15 +56,19 @@ void loop() {
   if (duty >= 0) {
     digitalWrite(dir, LOW);     // forward
     ledcWrite(pwm, duty);
-  }
-  else if (duty < 0) {
-
+  } else if (duty < 0) {
     digitalWrite(dir, HIGH);    // reverse
     ledcWrite(pwm, 1023 + duty);
   }
 
-  // Serial.println("Duty Cycle = " + String(duty / 1023.0));
-  double degrees = (encoder.getCount() / (64.0 * 18.75)) * 360.0;
-  Serial.println("Encoder degrees = " + String(degrees));
+  currentCount = encoder.getCount();
+  currentTime = micros() / 1e6f;
+  float dAngle = ((currentCount - prevCount) / countsPerRev) * TWO_PI;
+  float dTime = currentTime - prevTime;
+  currentVel = dAngle / dTime;
+  prevCount = currentCount;
+  prevTime = currentTime;
+  prevVel = currentVel;
+  Serial.println("Current_Velocity: " + String(currentVel));
   delay(100);
 }
